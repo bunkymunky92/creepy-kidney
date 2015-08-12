57b34b552c2f85178e35f13d344f7708bd74e2cc
rubyist <scott.barron@github.com>
2015-08-11 21:12:00 -0400

    slot click events, cancel events

--- src/shoptest/ShopTest.java
+++ src/shoptest/ShopTest.java
@@ -9,10 +9,21 @@ import net.canarymod.api.world.World;
 import net.canarymod.api.world.position.Location;
 import net.canarymod.api.world.blocks.BlockType;
 import net.canarymod.api.inventory.Inventory;
+import net.canarymod.api.inventory.ItemType;
+import net.canarymod.hook.HookHandler;
+import net.canarymod.hook.player.InventoryHook;
+import net.canarymod.hook.player.SlotClickHook;
+import net.canarymod.plugin.PluginListener;
 import com.pragprog.ahmine.ez.EZPlugin;
 import net.canarymod.api.factory.CanaryObjectFactory;
 
-public class ShopTest extends EZPlugin {
+public class ShopTest extends EZPlugin implements PluginListener {
+
+  @Override
+  public boolean enable() {
+    Canary.hooks().registerListener(this, this);
+    return super.enable();
+  }
   
   @Command(aliases = { "shoptest" },
             description = "shoptest plugin",
@@ -23,9 +34,21 @@ public class ShopTest extends EZPlugin {
       Player me = (Player)caller;
 
       CanaryObjectFactory of = new CanaryObjectFactory();
-      Inventory ci = of.newCustomStorageInventory(2);
+      Inventory ci = of.newCustomStorageInventory(1);
       ci.setInventoryName("WELCOME TO THE SHOP!!!");
+      ci.setSlot(ItemType.Arrow, 23, 2);
       me.openInventory(ci);
     }
+  }
+
+  @HookHandler
+  public void onInventory(InventoryHook event) {
+    Canary.instance().getServer().broadcastMessage("got an inventory event!");
+  }
+
+  @HookHandler
+  public void onSlotClick(SlotClickHook event) {
+    Canary.instance().getServer().broadcastMessage("got a slot click event!");
+    event.setCanceled();
   }
 }
